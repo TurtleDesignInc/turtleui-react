7c97b36b301a30600663322ac4b99c929327ef46
Vitor Ussui <vitor@turtleinc.com>
2025-02-21 15:53:12 -0300

    feat: update tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,233 @@
+import { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/stories/**/*.{ts,tsx,js,jsx}",
+  ],
+  theme: {
+    spacing: {
+      "150": "var(--values-150-rem)",
+      "120": "var(--values-120-rem)",
+      "100": "var(--values-100-rem)",
+      "78": "var(--values-78-rem)",
+      "72": "var(--values-72-rem)",
+      "64": "var(--values-64-rem)",
+      "56": "var(--values-56-rem)",
+      "48": "var(--values-48-rem)",
+      "40": "var(--values-40-rem)",
+      "36": "var(--values-36-rem)",
+      "32": "var(--values-32-rem)",
+      "28": "var(--values-28-rem)",
+      "24": "var(--values-24-rem)",
+      "20": "var(--values-20-rem)",
+      "18": "var(--values-18-rem)",
+      "16": "var(--values-16-rem)",
+      "14": "var(--values-14-rem)",
+      "12": "var(--values-12-rem)",
+      "10": "var(--values-10-rem)",
+      "8": "var(--values-8-rem)",
+      "6": "var(--values-6-rem)",
+      "4": "var(--values-4-rem)",
+      "2": "var(--values-2-rem)",
+      "1": "var(--values-1-rem)",
+    },
+    fontFamily: {
+      headers: ["var(--font-family-acid-grotesk)", "sans-serif"],
+      body: ["var(--font-family-inter)", "sans-serif"],
+    },
+    fontSize: {
+      base: "var(--values-16-rem)",
+      "100": "var(--values-100-rem)",
+      "64": "var(--values-64-rem)",
+      "56": "var(--values-56-rem)",
+      "48": "var(--values-48-rem)",
+      "40": "var(--values-40-rem)",
+      "32": "var(--values-32-rem)",
+      "24": "var(--values-24-rem)",
+      "20": "var(--values-20-rem)",
+      "18": "var(--values-18-rem)",
+      "16": "var(--values-16-rem)",
+      "14": "var(--values-14-rem)",
+      "12": "var(--values-12-rem)",
+      "10": "var(--values-10-rem)",
+    },
+    lineHeight: {
+      base: "1",
+      "100": "var(--values-100-rem)",
+      "72": "var(--values-72-rem)",
+      "64": "var(--values-64-rem)",
+      "56": "var(--values-56-rem)",
+      "48": "var(--values-48-rem)",
+      "40": "var(--values-40-rem)",
+      "32": "var(--values-32-rem)",
+      "28": "var(--values-28-rem)",
+      "24": "var(--values-24-rem)",
+      "20": "var(--values-20-rem)",
+      "18": "var(--values-18-rem)",
+      "16": "var(--values-16-rem)",
+      "14": "var(--values-14-rem)",
+      "12": "var(--values-12-rem)",
+      "10": "var(--values-10-rem)",
+    },
+    colors: {
+      button: {
+        primary: {
+          text: "var(--button-primary-text)",
+          icon: "var(--button-primary-icon)",
+          default: {
+            "bg-fill": "var(--button-primary-default-bg-fill)",
+          },
+          hover: {
+            "bg-fill": "var(--button-primary-hover-bg-fill)",
+          },
+          pressed: {
+            "bg-fill": "var(--button-primary-pressed-bg-fill)",
+          },
+          disabled: {
+            "bg-fill": "var(--button-primary-disabled-bg-fill)",
+          },
+        },
+        secondary: {
+          text: "var(--button-secondary-text)",
+          icon: "var(--button-secondary-icon)",
+          default: {
+            "bg-fill": "var(--button-secondary-default-bg-fill)",
+          },
+          hover: {
+            "bg-fill": "var(--button-secondary-hover-bg-fill)",
+          },
+          pressed: {
+            "bg-fill": "var(--button-secondary-pressed-bg-fill)",
+          },
+          disabled: {
+            "bg-fill": "var(--button-secondary-disabled-bg-fill)",
+          },
+        },
+        disabled: {
+          text: "var(--button-disabled-text)",
+        },
+      },
+
+      transparent: "transparent",
+      black: "var(--color-neutrals-1000)",
+      white: "hsl(var(--white))",
+      foreground: "hsl(var(--black))",
+      background: "hsl(var(--white))",
+      tan: "hsl(var(--tan))",
+      grey: {
+        lighter: "hsl(var(--grey-lighter))",
+        light: "hsl(var(--grey-light))",
+        DEFAULT: "hsl(var(--grey))",
+      },
+      turtle: {
+        blue: {
+          light: "hsl(var(--turtle-blue-light))",
+          DEFAULT: "hsl(var(--turtle-blue))",
+          dark: "hsl(var(--turtle-blue-dark))",
+        },
+      },
+      red: {
+        light: "hsl(var(--red-light))",
+        DEFAULT: "hsl(var(--red))",
+      },
+      green: {
+        light: "hsl(var(--green-light))",
+        DEFAULT: "hsl(var(--green))",
+      },
+      yellow: {
+        light: "hsl(var(--yellow-light))",
+        DEFAULT: "hsl(var(--yellow))",
+      },
+      purple: {
+        light: "hsl(var(--purple-light))",
+        DEFAULT: "hsl(var(--purple))",
+      },
+      teal: {
+        light: "hsl(var(--teal-light))",
+        DEFAULT: "hsl(var(--teal))",
+      },
+      custom: {
+        blue: "hsl(var(--custom-blue))",
+        yellow: "hsl(var(--custom-yellow))",
+        teal: "hsl(var(--custom-teal))",
+      },
+      accent: "hsl(var(--accent))",
+      popover: "hsl(var(--popover))",
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+    },
+    extend: {
+      spacing: {
+        button: "var(--values-12-rem)",
+      },
+      padding: {
+        "button-horizontal": "var(--values-24-rem)",
+        "button-vertical": "var(--values-16-rem)",
+      },
+
+      borderRadius: {
+        button: "var(--values-12-rem)",
+      },
+      boxShadow: {
+        x: "inset 35px 60px -15px rgba(0, 0, 0, 0.3)",
+        card: "0px 4px 8px 0px rgba(0, 0, 0, 0.16)",
+        inverted: "0 -4px 8px 0px rgba(0, 0, 0, 0.08)",
+      },
+      backgroundImage: {
+        "navbar-horizontal":
+          "linear-gradient(315deg, hsla(var(--turtle-blue), 0.2) 0%, hsla(var(--turtle-blue-light), 0.2) 100%)",
+        "navbar-vertical":
+          "linear-gradient(322deg, hsla(var(--turtle-blue), 0.2) 0%, hsla(var(--turtle-blue-light), 0.2) 100%)",
+        "join-hero":
+          "linear-gradient(0deg, #FFF 0%, rgba(135, 152, 224, 0.64) 110.01%);",
+      },
+      gridTemplateRows: {
+        "0fr": "0fr",
+        "1fr": "1fr",
+      },
+      height: {
+        em: "1em",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+      lineHeight: {
+        regular: "normal",
+      },
+      maxWidth: {
+        screen: "100vw",
+      },
+      width: {
+        em: "1em",
+      },
+      zIndex: {
+        5: "5",
+        15: "15",
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+    },
+  },
+  plugins: [animatePlugin],
+};
+
+export default config;
