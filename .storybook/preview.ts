190674145ae92d6a3bc67a7f01ac84d3bd534ecd
Vitor Ussui <vitor@turtleinc.com>
2025-02-21 15:52:35 -0300

    feat: set up chromatic

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,13 +3,19 @@ import "../src/styles.css";
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      toc: true, // 👈 Enables the table of contents
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    layout: "centered",
   },
+
+  tags: ["autodocs"],
 };
 
 export default preview;
